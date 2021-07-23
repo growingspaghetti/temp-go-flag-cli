9dc3aa7538c154705c8b5f6da355a2e58bd042c5
Ryoji <ryoji.kodakari@gmail.com>
2021-07-24 05:18:15 +0900

    アプリのGit情報を埋め込む

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+var (
+	version  string
+	commitId string
+)
+
 func main() {
 	fmt.Println("abc")
+	fmt.Println("Version:", version)
+	fmt.Println("Commit id:", commitId)
 }
